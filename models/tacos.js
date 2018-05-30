4716192d3b1b876b29bc960a8c7d6ea041488e2a
ektravel <38017980+ektravel@users.noreply.github.com>
2018-05-29 19:27:44 -0500

    added functionality to interact with the database

--- models/tacos.js
+++ models/tacos.js
@@ -3,9 +3,21 @@ var orm = require ("../config/orm.js");
 
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 var taco = {
-    
-}
-
-
+    selectAll: function(cb){
+        orm.selectAll("tacos", function(res){
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb){
+        orm.insertOne("tacos",cols, vals, function(res){
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb){
+        orm.updateOne("tacos", objColVals, condition, function(res){
+            cb(res);
+        });
+    }
+};
 
 module.exports = tacos;
