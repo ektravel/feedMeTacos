a3025049746bf4230ee63bf9e74ed63ab2bb9d8a
ektravel <38017980+ektravel@users.noreply.github.com>
2018-05-30 01:57:36 -0500

    added code for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 //Set up mySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password:"",
+//     database: "tacos_db"
+// });
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password:"",
     database: "tacos_db"
-});
+    });
+};
 
 //Make connection.
 connection.connect(function(err){
